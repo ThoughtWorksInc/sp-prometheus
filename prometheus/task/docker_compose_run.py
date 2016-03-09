efa62f8945a90633773f56dc5de0baac92387acf
yeyuexia <yyxworld@gmail.com>
2016-03-09 17:31:06 +0800

    [TECH][liuzhuo]: add —abort-on-container-exit when run docker-compose

--- prometheus/task/docker_compose_run.py
+++ prometheus/task/docker_compose_run.py
@@ -10,7 +10,10 @@ class Task:
     def run(self, compose_file, **kwargs):
         yaml_file = os.path.join(self.env.prometheus_path, compose_file)
         print "start run docker-compose: " + yaml_file
-        out, err = Popen(["docker-compose", "-f", yaml_file, "up"], stdout=PIPE, stdin=PIPE, stderr=PIPE).communicate()
+        out, err = Popen(
+            ["docker-compose", "-f", yaml_file, "up", "—abort-on-container-exit"],
+            stdout=PIPE, stdin=PIPE, stderr=PIPE
+        ).communicate()
         print out
         if err:
             print "error: " + err
