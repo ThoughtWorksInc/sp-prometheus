dd040cdadf95092950c405eee9ff35ea05c1a102
yeyuexia <yyxworld@gmail.com>
2016-04-18 16:58:14 +0800

    [TECH][liuzhuo]: add DOCKER_HOST into lsenviron

--- prometheus/task/docker_compose_run.py
+++ prometheus/task/docker_compose_run.py
@@ -1,5 +1,5 @@
 # coding: utf8
-import os.path
+import os
 from subprocess import call, PIPE, Popen
 
 
@@ -10,6 +10,7 @@ class Task:
     def run(self, compose_file, **kwargs):
         yaml_file = os.path.join(self.env.prometheus_path, compose_file)
         print "start run docker-compose: " + yaml_file
+        os.environ["DOCKER_HOST"] = self.env.docker_host
         out, err = Popen(["docker-compose", "-f", yaml_file, "up", "--abort-on-container-exit"]).communicate()
         if err:
             raise RuntimeError()
@@ -18,6 +19,7 @@ class Task:
             ["docker-compose", "-f", yaml_file, "rm", "-f"],
             stdout=PIPE, stdin=PIPE, stderr=PIPE
         ).communicate()
+        del os.environ["DOCKER_HOST"]
         print out
         # if err:
         #     print "error: " + err
